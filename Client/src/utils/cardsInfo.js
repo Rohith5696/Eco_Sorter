64da7e67ae551d335c61403e1efa39043edfe2de
Rohith5696 <rohithsiddhu7@gmail.com>
2024-12-17 17:43:28 +0530

    Add files via upload

---
+++ Client/src/utils/cardsInfo.js
@@ -0,0 +1,42 @@
+import bill from "../assets/bill.jpg";
+import elon from "../assets/elon.jpg";
+import pichai from "../assets/pichai.jpg";
+import user from "../assets/user.jpg";
+import ireda from "../assets/ireda.jpg";
+import green from "../assets/green.jpg";
+
+const cards = [
+  {
+    url: user,
+    name: "Shreya Sharma",
+    title: "Product User",
+    review:
+      "The website is very helpful and convenient. It also gives points and rewards for sorting correctly, which motivates to do better. A lot of learning about waste management and sustainability has happened from using this website.",
+    id: 1,
+  },
+  {
+    url: pichai,
+    name: "Sundar Pichai",
+    title: "CEO, Alphabet",
+    review:
+      "The innovative use of machine learning to sort waste into different categories is impressive. The website is easy to use, fast, and accurate. It also provides useful feedback and tips on how to reduce waste and recycle more. This is a great example of how technology can help us solve environmental problems.",
+    id: 2,
+  },
+  {
+    url: ireda,
+    name: "Prateek Aggarwal",
+    title: "IREDA",
+    review:
+      "Supporting such a noble and impactful initiative is a matter of pride. The website collects valuable data for further research and improvement. The website has a large and growing user base, which shows its popularity and effectiveness. This website has a bright future and will make a positive difference in the world.",
+    id: 3,
+  },
+  {
+    url: green,
+    name: "Esha",
+    title: "Green Person",
+    review:
+      "The website's mission and vision are appreciated. The website aims to reduce waste generation, increase recycling rates, and promote circular economy. The website also raises awareness and inspires action among people to adopt more eco-friendly habits and lifestyles. The website is a powerful tool for environmental protection and conservation.",
+    id: 4,
+  },
+];
+export default cards;
